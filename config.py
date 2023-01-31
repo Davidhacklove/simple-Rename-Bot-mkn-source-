390064c6de241860c5138205530acbb570e14910
Davidhacklove <94181522+Davidhacklove@users.noreply.github.com>
2023-01-31 20:49:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "12852064")) 
+API_HASH = environ.get("API_HASH", "7f7457395d064a4cc00e4f4a894f32d3")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6141807178:AAGh5SJeykah6D8IXTrKPOOYot3CviKEg5E")
+ADMIN = int(environ.get("ADMIN", "979749820"))          
+CAPTION = environ.get("CAPTION", "@filmthugs")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://telegra.ph/file/7924c7fc2e50a63d0a23b.jpg")
